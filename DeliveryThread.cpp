9f32c1451bfc06b9f54616f436123bd2d8811246
Daniel Fitchmun <danielfitchmun@ucrwpa4-8-124-42.wnet.ucr.edu>
2018-01-13 12:50:22 -0800

    changed to only bind to local

--- DeliveryThread.cpp
+++ DeliveryThread.cpp
@@ -21,7 +21,7 @@ void DeliveryThread(){
 	}
 	
 	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_addr.s_addr = inet_addr("127.0.0.1");
 	address.sin_port = htons( 2048 );
 	
 	/* bind to port 2048 */
--- DeliveryThread.hpp
+++ DeliveryThread.hpp
@@ -5,6 +5,7 @@
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <string.h>
 #include <unistd.h>
 #include <iostream>
