d25580e5a229351e2ef5479c0092c503294e7cc0
Daniel Fitchmun <danielfitchmun@thingy.local>
2018-02-07 13:24:15 -0800

    Changed port

--- pullDownTester.cpp
+++ pullDownTester.cpp
@@ -30,7 +30,7 @@ int main(){
 	
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = inet_addr("127.0.0.1");
-	address.sin_port = htons( 2048 );
+	address.sin_port = htons( 2418 );
 	
 	connect(recSocket, (struct sockaddr *)&address, addrlen);
 
